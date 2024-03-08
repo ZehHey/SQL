ee371b3ee46dfce0f57923564fb998f974aa0a4e
José C B Junior <142643035+ZehHey@users.noreply.github.com>
2024-03-08 00:45:49 -0300

    Aula08 e Aula9

---
+++ Aula08/comando_insert.sql
@@ -0,0 +1,7 @@
+insert into cliente values (2, 'Lucinea Hey', '00000000000', '1973-04-17', 03);
+insert into cliente values (3, 'Zeh Hey', '12345678912', '2024-01-05', 02);
+insert into cliente values (4, 'Edilson André Cardoso', '11122233344', '1992-12-30', 01);
+insert into cliente values (5, 'Adilson André Cardoso', '99988877766', '1962-11-29', 02);
+insert into cliente values (6, 'Edificio Colina do Estoril', '33333333333', '1986-01-01', 01);
+
+select * from cliente
